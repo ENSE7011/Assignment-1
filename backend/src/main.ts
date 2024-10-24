800041716661137768a4bb150829c0a365252696
nomiDwigg <naomiwiggins@me.com>
2024-10-24 14:55:08 +1300

    feat/update ui and claims

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({
+    origin: 'https://assign-1-frontend-preview.vercel.app/',
+    credentials: true,
+  });
   app.use(bodyParser.json());
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`server running on port ${port}`));
