0db69b322a5425b1093c4426ec60ae706098e3dc
nomiDwigg <naomiwiggins@me.com>
2024-10-24 14:44:42 +1300

    feat/fix claims import component

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: false, credentials: false });
+  app.enableCors({ origin: true, credentials: true });
   app.use(bodyParser.json());
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`server running on port ${port}`));
